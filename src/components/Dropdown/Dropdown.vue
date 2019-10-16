c3296cc1d7ea2d94e9d75e76a6b34f144423892b
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-16 15:25:26 -0400

    Wrap up dropdown styling

--- src/components/Dropdown/Dropdown.vue
+++ src/components/Dropdown/Dropdown.vue
@@ -1,19 +1,31 @@
 <template>
 	<div class="dropdown">
 		<label :for="title">{{ title }}</label>
-		<select name="">
-			<option
-				v-for="(option, index) in options"
-				:key="index"
-				:value="option"
-				>{{ option }}</option
-			>
-		</select>
+		<div class="dropdown__content">
+			<div class="dropdown__active">
+				<span>{{ active }}</span
+				><i class="fas fa-chevron-down"></i>
+			</div>
+			<ul class="dropdown__options">
+				<li
+					class="dropdown__option"
+					v-for="(option, index) in options"
+					:key="index"
+				>
+					{{ option }}
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	props: ['title', 'options']
+	props: ['title', 'options'],
+	data: function() {
+		return {
+			active: this.options[0]
+		};
+	}
 };
 </script>
