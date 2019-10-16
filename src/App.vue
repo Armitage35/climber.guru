e6aec84ff8fe1a6dcb1f9e25523d0975095e548c
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-16 11:57:12 -0400

    Add button component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-	<div id="app"></div>
+	<div id="app">
+		<Button type="primary" content="primary"></Button>
+		<Button type="secondary" content="secondary"></Button>
+	</div>
 </template>
 
 <script>
 import './Master.scss';
 
+import Button from './components/Button/Button';
+
 export default {
 	name: 'app',
-	components: {},
+	components: { Button }
 };
 </script>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,14 @@
+<template>
+	<button type="button" :class="classResolver">{{ content }}</button>
+</template>
+
+<script>
+export default {
+	props: ['content', 'type'],
+	computed: {
+		classResolver: function() {
+			return 'bttn__' + this.type;
+		}
+	}
+};
+</script>
