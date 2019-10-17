3820250925e9d06a12fbb4721a322145f7e177b6
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-17 09:15:21 -0400

    Boilerplate climbing card

---
+++ src/components/ClimbCard/ClimbCard.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="climbCard">
+		<div class="climbCard__icon">
+			<i class="fas fa-fill"></i>
+		</div>
+		<div class="climbCard__dropdowns">
+			<Dropdown
+				title="climbing type"
+				type="text"
+				:options="climbPerformance"
+			></Dropdown>
+			<Dropdown
+				title="climbing type"
+				type="text"
+				:options="boulderLevels"
+			></Dropdown>
+		</div>
+	</div>
+</template>
+
+<script>
+import Dropdown from '../Dropdown/Dropdown';
+
+export default {
+	components: { Dropdown },
+	data: function() {
+		return {
+			climbPerformance: ['Onsight', 'Flash', 'Redpoint', 'Repeat'],
+			level: []
+		};
+	},
+	computed: {
+		boulderLevels: function() {
+			let boulderLevel = [];
+			for (let i = 0; i < 15; i++) {
+				boulderLevel.push('V' + i);
+			}
+			return boulderLevel;
+		}
+	}
+};
+</script>
--- src/components/Modal/NewSession/NewSession.vue
+++ src/components/Modal/NewSession/NewSession.vue
@@ -1,25 +1,30 @@
 <template>
 	<div>
 		<div class="modal__body">
-			<Dropdown
-				title="climbing type"
-				type="text"
-				:options="climbTypes"
-				icon="fas fa-map-marker-alt"
-			></Dropdown>
-			<Dropdown
-				title="session date"
-				type="date"
-				icon="far fa-calendar-alt"
-				:options="dropdown"
-			></Dropdown>
-			<Dropdown
-				title="Climbing location"
-				type="text"
-				icon="fas fa-mountain"
-				:options="climbGym"
-			></Dropdown>
+			<div class="newSession__drpodowns">
+				<Dropdown
+					title="climbing type"
+					type="text"
+					:options="climbTypes"
+					icon="fas fa-map-marker-alt"
+				></Dropdown>
+				<Dropdown
+					title="session date"
+					type="date"
+					icon="far fa-calendar-alt"
+					:options="dropdown"
+				></Dropdown>
+				<Dropdown
+					title="Climbing location"
+					type="text"
+					icon="fas fa-mountain"
+					:options="climbGym"
+				></Dropdown>
+			</div>
 			<label>What did you climb today?</label>
+			<div class="newSession_climbs">
+				<ClimbCard></ClimbCard>
+			</div>
 		</div>
 		<div class="modal__footer">
 			<Button
@@ -35,8 +40,10 @@
 <script>
 import Button from '../../Button/Button';
 import Dropdown from '../../Dropdown/Dropdown';
+import ClimbCard from '../../ClimbCard/ClimbCard';
+
 export default {
-	components: { Button, Dropdown },
+	components: { Button, ClimbCard, Dropdown },
 	props: ['actions'],
 	data: function() {
 		return {
