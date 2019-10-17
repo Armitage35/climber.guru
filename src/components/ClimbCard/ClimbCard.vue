307e0d3858270a52ed0bdee13dde6696c199c1ef
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-17 09:43:21 -0400

    Introduce text input as a component

--- src/components/ClimbCard/ClimbCard.vue
+++ src/components/ClimbCard/ClimbCard.vue
@@ -15,14 +15,26 @@
 				:options="boulderLevels"
 			></Dropdown>
 		</div>
+		<div class="climbCard__footer">
+			<div class="climbingCard__picture">
+				<i class="fas fa-camera-retro"></i>
+			</div>
+			<div class="climbingCard__routeName">
+				<TextInput
+					name="routeName"
+					placeholder="Route name"
+				></TextInput>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 import Dropdown from '../Dropdown/Dropdown';
+import TextInput from '../TextInput/TextInput';
 
 export default {
-	components: { Dropdown },
+	components: { Dropdown, TextInput },
 	data: function() {
 		return {
 			climbPerformance: ['Onsight', 'Flash', 'Redpoint', 'Repeat'],
---
+++ src/components/TextInput/TextInput.vue
@@ -0,0 +1,18 @@
+<template>
+	<input type="text" :name="name" :placeholder="placeholder" />
+</template>
+
+<script>
+export default {
+	props: {
+		name: {
+			type: String,
+			required: false
+		},
+		placeholder: {
+			type: String,
+			required: false
+		}
+	}
+};
+</script>
