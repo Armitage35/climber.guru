ec09411641fedc44b97c7d443ebcff186d41e34d
Adrien D. Ahlqvist <adrien.dubois35@gmail.com>
2019-10-29 09:34:25 -0400

    Split state mngt in separate files

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import modal from './modules/modal';
+
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
 	state: {
 		userID: 1,
-		modal: true,
 		isLoading: true,
 		climbPerformances: [],
 		userPreferences: {},
@@ -16,9 +17,6 @@ export const store = new Vuex.Store({
 	getters: {
 		getUserID: state => {
 			return state.userID;
-		},
-		getModalState: state => {
-			return state.modal;
 		},
 		getAppLoadingStatus: state => {
 			return state.isLoading;
@@ -30,15 +28,18 @@ export const store = new Vuex.Store({
 			return state.routeGrades;
 		},
 		getClimbingGrades: state => {
-			return { ...state.boulderingGrades, ...state.routeGrades };
+			return {
+				...state.boulderingGrades,
+				...state.routeGrades
+			};
 		}
 	},
 	mutations: {
-		toggleModal: state => {
-			state.modal = !state.modal;
-		},
 		toggleLoadingState: state => {
 			state.isLoading = !state.isLoading;
 		}
+	},
+	modules: {
+		modal
 	}
 });
